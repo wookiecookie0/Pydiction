37e97106550033875aca47f3a8cb25ed779ccc8a
wookiecookie0 <Hollowayt9182@gmail.com>
2017-09-12 17:55:22 -0400

    Updated some stuff

--- PathToVictory.py
+++ PathToVictory.py
@@ -7,18 +7,24 @@ import os
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('nfl_games_2013.csv', index_col=['date'], parse_dates=['date'], infer_datetime_format=True)
-first = df.ix[0,0]
+#df = pd.read_csv('nfl_games_2013.csv', index_col=['date'], parse_dates=['date'], infer_datetime_format=True)
+df = pd.read_csv('nfl_games_2013.csv', parse_dates=['date'], infer_datetime_format=True)
+playoff_mask = df['playoff'] == 0
+df = df[playoff_mask]
+first = df.date[0]
 last = first + pd.offsets.Day(7)
+df['Week'] = 0
 mask = ((df['date'] >= first) & (df['date']<last))
-test = df[mask]
+#test = df[mask]
 year = str(df.season[0])
-os.makedirs(year,exist_ok=True)
-print(test)
+#os.makedirs(year,exist_ok=True)
+#print(test)
 for i in range(17):
     mask = ((df['date'] >= first) & (df['date'] < last))
-    test = df[mask]
-    new_df=pd.DataFrame(test,)
-    test.to_csv((year+'/'+'Week ' + str(i+1) + '.csv'))
+    df.Week[mask] = i+1
+    print(df[mask])
+    #new_df=pd.DataFrame(test,)
+    #test.to_csv((year+'/'+'Week ' + str(i+1) + '.csv'))
     first = last
     last = first + pd.offsets.Day(7)
+print(df.Week)
